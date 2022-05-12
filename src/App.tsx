00f0d08f1426b238e164d6640f13a277f94b0937
yousefomar724 <69850453+yousefomar724@users.noreply.github.com>
2022-05-12 19:51:58 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Accordion from "./components/accordion";
+
+export default function App() {
+  return (
+    <div className="container">
+      <h1 className="title">Frequesntly Asked Questions</h1>
+      <Accordion />
+    </div>
+  );
+}
